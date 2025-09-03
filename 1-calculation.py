b4826098ec8b422803df4392d8c95133ab480295
Saniez-l <saniezl.christophe@gmail.com>
2025-09-03 13:44:49 +0200

    changing function print for requierment

--- 1-calculation.py
+++ 1-calculation.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print(f"{a} + {b} = {cal.add(a, b)}")
-    print(f"{a} + {b} = {cal.sub(a, b)}")
-    print(f"{a} + {b} = {cal.mul(a, b)}")
-    print(f"{a} + {b} = {cal.div(a, b)}")
+    print(f"{a} - {b} = {cal.sub(a, b)}")
+    print(f"{a} * {b} = {cal.mul(a, b)}")
+    print(f"{a} / {b} = {cal.div(a, b)}")
