abe7f0ad618c165e27276aa59d4d5dbb496e01aa
Saniez-l <saniezl.christophe@gmail.com>
2025-09-03 16:37:59 +0200

    add if name

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 import sys
+if __name__ == "__main__":
 
-argv = sys.argv[1:]
-argc = len(argv)
+    argv = sys.argv[1:]
+    argc = len(argv)
 
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("{} argument:".format(argc))
-else:
-    print("{} arguments:".format(argc))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
 
-for i, argv in enumerate(argv, start=1):
-    print("{}: {}".format(i, argv))
+    for i, argv in enumerate(argv, start=1):
+        print("{}: {}".format(i, argv))
