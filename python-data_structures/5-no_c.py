032191605cf0a6182e0efb983d3bc9421261592e
Saniez-l <saniezl.christophe@gmail.com>
2025-09-08 14:51:21 +0200

    add one condition

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = "".join([x for x in my_string if x != 'c' and 'C'])
+    new_string = "".join([x for x in my_string if x != 'c' and x != 'C'])
     return new_string
