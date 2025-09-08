4439cde96d8a696854a5ea299ac3c07b405572f7
Saniez-l <saniezl.christophe@gmail.com>
2025-09-08 14:46:06 +0200

    changing function from .join to replace

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = "".join([x for x in my_string if x != 'c' and 'C'])
+
+    for charactere in "cC":
+        my_string = my_string.replace(charactere, "")
+
     return my_string
