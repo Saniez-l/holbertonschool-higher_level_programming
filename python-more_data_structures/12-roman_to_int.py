91830884d4bb09b45146bc95cf21ffa3d5fae92a
Saniez-l <saniezl.christophe@gmail.com>
2025-09-09 23:16:52 +0200

    program converts a Roman numeral to an integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    if len(roman_string) == 0:
+        return 0
+    a_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    valeur = []
+    for key in roman_string:
+        valeur.append(a_dic[key])
+
+    return sum(valeur)
