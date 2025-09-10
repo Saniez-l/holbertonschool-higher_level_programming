33a5213bb6c6209a1a28b3393ba4e9f176de1c97
Saniez-l <saniezl.christophe@gmail.com>
2025-09-10 14:05:15 +0200

    function that divides 2 integers and prints the result,except if divides by 0

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    resultat = None
     try:
         resultat = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
         return None
     else:
-        print("Inside result: {:.1f}".format(resultat))
         return resultat
+    finally:
+        if resultat is None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {:.1f}".format(resultat))
