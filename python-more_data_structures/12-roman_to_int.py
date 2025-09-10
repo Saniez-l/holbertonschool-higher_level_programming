47dac3b8199a926d61412108ec79eee76006ab10
Saniez-l <saniezl.christophe@gmail.com>
2025-09-10 09:16:56 +0200

    add condtion if not string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,9 @@
 
 def roman_to_int(roman_string):
 
+    if type(roman_string) != str:
+        return 0
+
     if len(roman_string) == 0:
         return 0
     a_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
