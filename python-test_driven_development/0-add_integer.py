2ed5aa2c8a37f19b837958d1a2009202ac4edce7
Saniez-l <saniezl.christophe@gmail.com>
2025-09-11 09:28:26 +0200

    add condition for float

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,11 +17,11 @@ def add_integer(a, b=98):
         int: sum of two number
 
     Exceptions:
-        TypeError: if a or b is not integer or gloat
+        TypeError: if a or b is not integer or float
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer or float")
 
     return int(a) + int(b)
