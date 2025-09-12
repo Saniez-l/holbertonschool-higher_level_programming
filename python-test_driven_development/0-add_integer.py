6db4737cb63e5bf8af5b44a16ef4c16a542d8b06
Saniez-l <saniezl.christophe@gmail.com>
2025-09-12 11:27:20 +0200

    delete excess line

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 """
 Module 0-add_integer
-contain fonction add_integer add two integer.
+contain fonction 
+add_integer add two integer.
 """
 
 
 def add_integer(a, b=98):
     """
     add two integer or float.
-
-    Args:
-        a (int ou float): first number
-        b (int ou float, optionnel): second number by default (98)
-
-    Returns:
-        int: sum of two number
-
-    Exceptions:
-        TypeError: if a or b is not integer or float
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
