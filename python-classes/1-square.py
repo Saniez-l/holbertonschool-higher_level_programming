a32bdf064a73a351b467df66b3fbb971b3ffb80a
Saniez-l <saniezl.christophe@gmail.com>
2025-09-15 11:19:08 +0200

    intialisation private instantance attribue size for class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""
+This module contain a class Square
+"""
+
+
+class Square:
+    """"
+    this class contain class Square
+    """
+    def __init__(self, size):
+        """
+        intitialisation square with size
+        """
+        self.__size = size
