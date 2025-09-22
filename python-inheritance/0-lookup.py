23cdfa790cbcba114a5135e0de8f35f793dd6abd
Saniez-l <saniezl.christophe@gmail.com>
2025-09-22 15:23:17 +0200

    add dicstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides the function `lookup`
+that returns the list of available attributes and methods of an object.
+"""
+
 
 def lookup(obj):
     """
-    function that returns the list of
-    available attributes and methods of an object
+    Return the list of available attributes and methods of the given object.
+
+    Args:
+        obj: Any Python object.
+
+    Returns:
+        list: A list of attribute and method names.
     """
     return dir(obj)
