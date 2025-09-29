397f0b0d3a4baaf200a29de7558a044d40f19733
Saniez-l <saniezl.christophe@gmail.com>
2025-09-29 10:10:57 +0200

    add encodings UTF8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,9 +5,9 @@ Module for read file
 
 def read_file(filename=""):
     """
-    Read file with open
+    Read file with open and encodings UTF8
     """
 
-    with open(filename, "r") as fichier:
+    with open(filename, "r", encoding="UTF8") as fichier:
         contenu = fichier.read()
         print(contenu.strip(),end="")
