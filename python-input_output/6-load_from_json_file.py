cd56529478d115a6bfb2a76f91c346e53028048e
Saniez-l <saniezl.christophe@gmail.com>
2025-09-29 15:14:56 +0200

    function load from json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that creates an Object from a “JSON file"
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a jason file
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
