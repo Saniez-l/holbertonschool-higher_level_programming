fe627451f88280a26e4c000772423e208ef1f8ef
Saniez-l <saniezl.christophe@gmail.com>
2025-10-01 22:41:04 +0200

    Pickling Custom Classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Pickling Custom Classes
+"""
+import pickle
+
+
+class CustomObject:
+    """
+    Class customobject
+    """
+
+    def __init__(self, name, age, is_student):
+        """
+        Init name, age, is_student
+        """
+        if not isinstance(name, str):
+            raise TypeError("Name is not string")
+        if not isinstance(age, int):
+            raise TypeError("age is not integer")
+        if not isinstance(is_student, bool):
+            raise TypeError("is_student is not boolean")
+
+        self.Name = name
+        self.Age = age
+        self.Is_student = is_student
+
+    def display(self):
+        """
+        Method to print out the object’s attributes with the following format
+        """
+        for attr, value in self.__dict__.items():
+            print(f"{attr}: {value}")
+
+    def serialize(self, filename):
+        """
+        Method to serialisation with pickle
+        """
+        if filename is None:
+            return None
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """
+        Method to deserialisation
+        """
+        try:
+            with open(filename, "rb") as f:
+                data = pickle.load(f)
+            return data
+
+        except (FileNotFoundError, pickle.UnpicklingError):
+            return None
