4e47a0055870016148f293126a43c1a668e13d96
Saniez-l <saniezl.christophe@gmail.com>
2025-10-07 16:20:46 +0200

    serveur using api

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Module for api server
+"""
+
+from http.server import BaseHTTPRequestHandler
+import socketserver
+import json
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    """
+    Class api server
+    """
+    def do_GET(self):
+        """
+        Module api seerver do_GET
+        """
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            sample_data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+
+            }
+            response = json.dumps(sample_data)
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(response.encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
