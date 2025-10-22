980fc039229a9d20fdba66e2640516324739c87e
Saniez-l <saniezl.christophe@gmail.com>
2025-10-22 16:33:29 +0200

    add argv

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+Script list all states
+"""
 
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(host="localhost",port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost",port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
 
