c361963639538d150f5cd565c10810ba75795f0e
Saniez-l <saniezl.christophe@gmail.com>
2025-11-07 17:12:26 +0100

    changing for requierment

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myVar.length; i++) {
+    console.log(myVar[i]);
+}
