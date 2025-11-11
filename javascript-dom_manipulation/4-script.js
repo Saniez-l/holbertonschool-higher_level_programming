1c22f3a6aaf279b277ce5d46099673f4c80fd606
Saniez-l <saniezl.christophe@gmail.com>
2025-11-11 22:54:57 +0100

    changing for upercase item

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -4,6 +4,6 @@ const list = document.querySelector('.my_list');
 const redHeader = document.querySelector('#add_item');
 redHeader.addEventListener('click', () => {
   const newItem = document.createElement('li');
-  newItem.textContent = 'item';
+  newItem.textContent = 'Item';
   list.append(newItem);
 });
