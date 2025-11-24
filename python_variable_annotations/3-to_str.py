a99fa35796ee7b1cf526709a674c2a1295bbdd83
Saniez-l <saniezl.christophe@gmail.com>
2025-11-24 16:29:30 +0100

    add file for task 0, 1, 2, 3, 4

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module that defines a type-annotated function to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """return str value of float"""
+    return str(n)
---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module that defines annotated variables with specific values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
